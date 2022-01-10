5605d53616179bbd35fb52ead532866de442fb0b
Kuba <jakubkozlowski.pl@gmail.com>
2022-01-10 22:23:49 +0100

    Finished 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -7,17 +8,29 @@ const Wrapper = styled.div`
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  color: '#4a6166';
 
-  h1,
+  h1 {
+    font-size: 150px;
+    margin: 0;
+    line-height: 1;
+  }
+
   h2 {
     margin: 0;
+  }
+
+  p {
+    text-align: center;
   }
 `
 
 const PageNotFound = () => (
   <Wrapper>
     <h1>404</h1>
-    <h2>Not found</h2>
+    <h2>Not Found</h2>
+    <p>The resource requested could not be found on this server.</p>
+    <Link to="/">Back to homepage.</Link>
   </Wrapper>
 )
 
